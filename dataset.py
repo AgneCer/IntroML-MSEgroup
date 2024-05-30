ea2a65e944ae4d0e1b978082b7284f78047c67d8
AgneCer <agnese.cervino@studenti.unitn.it>
2024-05-30 15:42:07 +0200

    config

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
 import os
 from PIL import Image
 
@@ -38,6 +39,19 @@ def get_data_flowers(batch_size_train, batch_size_test, num_workers, transform=N
 
     return train_loader, val_loader
 
+class CustomImageFolder(ImageFolder):
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        folder_name = os.path.basename(os.path.dirname(path))
+        target = folder_name.split('_')[0]
+        
+        return sample, target
 
 def get_data_loaders(data_dir, batch_size_train, batch_size_test, num_workers, train_transform=None, val_transform=None):
     
@@ -73,47 +87,3 @@ def get_data_loaders(data_dir, batch_size_train, batch_size_test, num_workers, t
     val_loader = DataLoader(val_dataset, batch_size=batch_size_test, shuffle=False, num_workers=num_workers)
 
     return train_loader, val_loader
-
-class CustomImageDataset(Dataset):
-    def __init__(self, labels, images, transform=None, target_transform=None):
-        self.img_labels = labels
-        self.img_paths = images
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __len__(self):
-        return len(self.img_labels)
-
-    def __getitem__(self, idx):
-        img_path = self.img_paths[idx]
-        image = Image.open(img_path).convert("RGB")
-        label = self.img_labels[idx]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
-    
-
-class CustomDatasetTrain:
-    def __init__(self, img_paths, img_labels, transform=None, target_transform=None):
-        self.img_paths = img_paths
-        self.img_labels = img_labels
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __getitem__(self, idx):
-        img_path = self.img_paths[idx]
-        image = Image.open(img_path).convert("RGB")
-        folder_name = os.path.basename(os.path.dirname(img_path))
-        label = folder_name.split('_')[0]
-
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-
-        return image, label
-
-    def __len__(self):
-        return len(self.img_paths)
