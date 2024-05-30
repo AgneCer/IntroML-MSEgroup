5426c0eaa837b16856d6dfe0b8fac5fea1733644
AgneCer <agnese.cervino@studenti.unitn.it>
2024-05-30 17:27:23 +0200

    submit

--- submit.py
+++ submit.py
@@ -2,6 +2,9 @@ from http.client import responses
 import requests
 import json
 import utils
+import argparse
+import torch
+import yaml
 
 def submit(results, url="https://competition-production.up.railway.app/results/"):
     res = json.dumps(results)
@@ -15,18 +18,29 @@ def submit(results, url="https://competition-production.up.railway.app/results/"
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--config", required=True, type=str, help="Path to the configuration file")
+    parser.add_argument("--run_name", required=False, type=str, help="Name of the run")
+    args = parser.parse_args()
 
-    model = None
+    torch.manual_seed(1234)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+    
+    model = config["model"]
     filename = None
     current_model = None
 
-    test_loader = None
-    device = None
+    mean = config["param"]["mean"]
+    sd = config["param"]["sd"]
+    #test_loader = (PASS mean and sd)
 
     best_model = utils.load_model(model, filename, current_model)
     
-    # Passa `keep_full_label=True` per mantenere le etichette complete (id_name)
-    # Passa `keep_full_label=False` per ottenere solo l'ID dalle etichette
+    # `keep_full_label=True` for complete lable (id_name)
+    # `keep_full_label=False` to get just the id
     preds = utils.return_predictions_dict(best_model, test_loader, device, keep_full_label=False)
 
     res = {
